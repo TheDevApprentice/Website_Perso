09fd8f2a1e1488e1af6997bb9df91e1a49ab6021
Hugo Abric <80226527+TheDevApprentice@users.noreply.github.com>
2022-02-11 10:35:25 -0500

    nice

--- Javascript/scrollreveal.js
+++ Javascript/scrollreveal.js
@@ -1,43 +1,46 @@
 var slideUp = {
     distance: '150%',
     origin: 'bottom',
-    opacity: null
+    opacity: null, 
+    reset: true
 };
 
 var slideLeft = {
     distance: '150%',
     origin: 'right',
-    opacity: null
+    opacity: null,
+    reset: true
 };
 var slideRight = {
     distance: '150%',
     origin: 'left',
-    opacity: null
+    opacity: null,
+    reset: true
 };
 var slideDown = {
     distance: '30%',
     origin: 'top',
-    opacity: null
+    opacity: null,
+    reset: true
 };
 
 ScrollReveal().reveal('.slide-up', slideUp);
 ScrollReveal().reveal('.slide-left', slideLeft);
 ScrollReveal().reveal('.slide-right', slideRight);
 ScrollReveal().reveal('.slide-down', slideDown);
-
 ScrollReveal().reveal('.slide-leftnav', slideLeft, { Duration : 500,  easing: 'ease-in'});
-ScrollReveal().reveal('#about');
+
 ScrollReveal().reveal('main');
 var slideRightFooter = {
     distance: '150%',
     origin: 'left',
     opacity: null
 };
-ScrollReveal().reveal('.footer-copyright .footer-navigation, .footer-social', slideRightFooter,{easing:'ease', Duration : 1000 });
 
-ScrollReveal().reveal('.attribution', slideLeft, { delay : 500 });
-
-ScrollReveal().reveal('#about', slideDown, { easing: 'ease-in', delay: 3500});
+ScrollReveal().reveal('#about', { easing: 'ease-in', delay: 100});
 ScrollReveal().reveal('#creations',{ delay: 500});
 ScrollReveal().reveal('#technologies',{ delay: 500});
 ScrollReveal().reveal('#more',{ delay: 500});
+ScrollReveal().reveal('#contact',{ delay: 500});
+ScrollReveal().reveal('.footer-copyright, .footer-navigation, .footer-social', slideRightFooter,{easing:'ease', delay : 1000 });
+ScrollReveal().reveal('.attribution', slideLeft, { delay : 1000 });
