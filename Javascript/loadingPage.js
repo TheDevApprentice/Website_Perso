c6f68a261962ba2e6c5cc4a2b63c727310a69904
Hugo Abric <80226527+TheDevApprentice@users.noreply.github.com>
2022-02-13 13:30:08 -0500

    finition de la loading page avec un timer

--- Javascript/loadingPage.js
+++ Javascript/loadingPage.js
@@ -1,3 +1,12 @@
 $(window).on("load", function() {
-    $("#loadingWrapper").fadeOut("2000");
+    $("#loadingWrapper").fadeOut(function() {
+        // fadeOut complete. Remove the loading div
+        setTimeout(removeLoader, 2500); //wait for page load PLUS two seconds.
+    });
 }); 
+function removeLoader(){
+    $( "#loadingWrapper" ).fadeOut(2500, function() {
+      // fadeOut complete. Remove the loading div
+      $( "#loadingWrapper" ).remove(); //makes page more lightweight 
+  });  
+}
